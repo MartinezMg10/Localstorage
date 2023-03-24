64c133e54c06e607725398146b8269a5b3630c22
BrayanLeon <juanagelves@gmail.com>
2023-03-24 11:27:57 -0500

    localstorage añadido

---
+++ main.js
@@ -0,0 +1,39 @@
+const agregar = document.getElementById('agregar')
+const producto = document.getElementById('producto')
+const precio = document.getElementById('precio')
+
+class Producto{
+    constructor(producto,precio){
+        this.producto = producto;
+        this.precio = precio;
+    }
+}
+
+let array = []
+
+agregar.addEventListener('click',()=>{
+    if(producto.value != '' && precio.value != ''){
+        const nuevoProducto = new Producto(producto.value,precio.value)
+        array.push(nuevoProducto)
+        console.log(nuevoProducto)
+        localStorage.setItem("productos", JSON.stringify(array));
+
+
+        producto.value=''
+        precio.value=''
+    }else{
+        alert("Faltan Campos")
+    }
+})
+
+
+
+window.addEventListener("load", ()=> {
+    const traerProductos = JSON.parse(localStorage.getItem("productos"));
+
+    traerProductos.forEach(element => {
+        let producto = new Producto(element.producto,element.precio)
+        array.push(producto)
+        console.log(traerProductos)
+    } )
+});
